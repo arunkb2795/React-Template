8742395ee88c8de1e0aed9ddb2a5d74a387deafb
arunkb2795 <arunkb27@gmail.com>
2021-03-24 12:57:19 +0530

    css styles image svg configration added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import IMAGE from './javascript.png'
+import SVG from './javascript.svg'
+import "./styles.css"
 export default function App() {
     return (
+        <>
         <h1>
             Hello World
-        </h1>
+            </h1>
+            <img src={IMAGE} alt="JS png" width="200" height="200" />
+            <img src={SVG} alt="JS svg" width="200" height="200" />
+        </>
     )
 }
