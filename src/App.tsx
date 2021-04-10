52ebb3ae2207c42f78c6acfa73f90806c2c9b757
arunkb2795 <arunkb27@gmail.com>
2021-04-10 08:12:38 +0530

    env setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import "./styles.css"
 export default function App() {
     return (
         <>
-        <h1>
-            Hello World
+            <h1>
+                Hello World {process.env.NODE_ENV}
             </h1>
             <img src={IMAGE} alt="JS png" width="200" height="200" />
             <img src={SVG} alt="JS svg" width="200" height="200" />
